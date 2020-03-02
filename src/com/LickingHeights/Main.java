86bd95bbb8403668ca891b1d900afaa35fbb1cc0
Eli Spicer <joshua.spicer.2020@student.lhschools.org>
2020-03-02 12:09:13 -0500

    Intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,105 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //Phase 1
+    String adjective1;
+    String place;
+    String adjective2;
+    String adjective3;
+    String pluralNoun1;
+    String pluralNoun2;
+    String noun1;
+    String verb1;
+    String noun2;
+    String verb2;
+    String actionVerb1;
+    String pluralNoun3;
+    String noun3;
+    String verbEndingInIng;
+    String noun4;
+    String measurementOfTime;
+    String adjective4;
+    String actionVerb2;
+    String verb3;
+    String adjective5;
+    String possessiveNoun;
+    String userName;
+    Scanner keyboard;
+    //Phase 2
+keyboard = new Scanner(System.in);
+System.out.println("Give me a describing word");
+adjective1 = keyboard.nextLine();
+System.out.println("Give me any place");
+place = keyboard.nextLine();
+System.out.println("Give me another describing word");
+adjective2 = keyboard.nextLine();
+System.out.println("Give me another describing word that starts with an r");
+adjective3 = keyboard.nextLine();
+System.out.println("Give me any thing that is plural");
+pluralNoun1 = keyboard.nextLine();
+System.out.println("Give me a plural noun");
+pluralNoun2 = keyboard.nextLine();
+System.out.println("Give me a body part");
+noun1 = keyboard.nextLine();
+System.out.println("Give me any verb");
+verb1 = keyboard.nextLine();
+System.out.println("Give me any animal name");
+noun2 = keyboard.nextLine();
+System.out.println("Give me any verb");
+verb2 = keyboard.nextLine();
+System.out.println("Give ma a action verb");
+actionVerb1 = keyboard.nextLine();
+System.out.println("Give me a noun that is plural");
+pluralNoun3 = keyboard.nextLine();
+System.out.println("Give me a persons name");
+noun3 = keyboard.nextLine();
+System.out.println("Give me a ver that ends in ing");
+verbEndingInIng = keyboard.nextLine();
+System.out.println("Give me a noun");
+noun4 = keyboard.nextLine();
+System.out.println("Give me a measurement of time");
+measurementOfTime = keyboard.nextLine();
+System.out.println("Give me another describing word");
+adjective4 = keyboard.nextLine();
+System.out.println("Give me an action verb");
+actionVerb2 = keyboard.nextLine();
+System.out.println("Give me a verb");
+verb3 = keyboard.nextLine();
+System.out.println("Give me a describing word");
+adjective5 = keyboard.nextLine();
+System.out.println("Give me a possessive noun");
+possessiveNoun = keyboard.nextLine();
+//Phase 3
+ System.out.println("On the "+adjective1+" trip to "+place+" my "+adjective2);
+System.out.println("friend and I decided to invent a game. Since this would be a rather "+adjective3);
+System.out.println("trip, it would need to be a game with "+pluralNoun1);
+System.out.println("and "+pluralNoun2+". Using our "+noun1+" to "+verb1);
+System.out.println("we tried to get the "+noun2+" next to us to play too, but they just "+verb2);
+System.out.println("ed at us and "+actionVerb1+" away. After a few rounds we thought the ");
+System.out.println("game could use some "+pluralNoun3+" so we turned on the "+noun3);
+System.out.println("and started "+verbEndingInIng+" to the "+noun4+" that came on. ");
+System.out.println("This lasted for "+measurementOfTime+" before I got "+adjective4+" and decided to "+actionVerb2);
+System.out.println(". Ill never "+verb3+" that trip, it was the "+adjective5+" road trip of my "+possessiveNoun);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
